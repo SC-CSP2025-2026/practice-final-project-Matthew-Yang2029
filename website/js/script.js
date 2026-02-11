748c036155a6353dfba948edb7e4009f95cc4f45
Matthew Yang <matthew.yang@scsstudent.org>
2026-02-10 22:43:04 -0800

    feat: Added an alert when array not set properly

--- website/js/script.js
+++ website/js/script.js
@@ -170,6 +170,10 @@ function displayTrueFalseAnswers(question) {
 
 // Shuffle array in place
 function shuffleArray(array) {
+  if (!array) {
+    alert("Array not set properly.");
+    return;
+  }
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
